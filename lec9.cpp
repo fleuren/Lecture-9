0bd0e57239894a2f8551487a66c5b72928178583
fleuren <91404708+fleuren@users.noreply.github.com>
2021-12-09 18:34:53 +0400

    Add files via upload

---
+++ lec9.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+/*
+cout << "What is the capital of France" << endl;
+cout << "a: Paris, b: Berlin, c: Croissant Land" << endl;
+char choice;
+cin >> choice;
+switch (choice){
+	case 'a':
+	cout << "You're correct!";
+	break;
+
+	case 'b':
+	cout << "You're incorrect";
+	break;
+
+	case 'c':
+	cout << "You're incorrect";
+	break;
+
+	default:
+	cout << "Invalid input";
+	break;
+}
+*/
+return 0;
+}
